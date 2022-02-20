7972aa5ca36607f7506142fb7d625ac19aef8312
CarlDanielJ <mar19340@uvg.edu.gt>
2022-02-20 01:02:45 -0600

    Conversion infix a postfix

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
@@ -14,7 +17,7 @@ public class Main {
 		 */
 
 		int num;
-		System.out.println("Biennvenidos al menú de seleccion");
+		System.out.println("Biennvenidos al menu de seleccion");
 		System.out.println("A continuacion le aparecerán las opciones de tipo de lista:");
 		System.out.println("------------------------------------------------------------");
 		System.out.println("1. Seleccionar ArrayLists")	;
@@ -46,5 +49,62 @@ public class Main {
 	    System.out.println(cadena);
 		cadena = miStack.pop();
 		System.out.println(cadena);
+		
+		
+		//---------------LEER EN NOTACION INFIX
+		//--------------DE ARCHIVO .txt
+		
+		ArrayList<String> lista = new ArrayList<>();
+		
+		 try(FileReader fileReader = new FileReader("Prueba.txt")){
+		        int caracterLeido = fileReader.read();
+		        while(caracterLeido!= -1) {
+		            char caracter = (char) caracterLeido;
+		            String caracter1 = String.valueOf(caracter);
+		            lista.add(caracter1);
+		            caracterLeido = fileReader.read();
+		        }
+		    }catch(IOException ex){
+		        System.err.println("Erorr al leer el archivo");
+		        ex.printStackTrace();
+
+		    }
+
+		 ArrayList<String>removed = new ArrayList<>();  
+		 removed.add(" ");
+		 
+		 lista.removeAll(removed);
+		 
+		 System.out.println("La operacion a realizar es la siguiente");
+		 System.out.println();
+
+		 System.out.println(lista);
+		 
+		 ArrayList<String> numeros = new ArrayList<>();
+		 ArrayList<String> opera = new ArrayList<>();
+		 
+		 for(String x:lista) {
+			 
+			 try {
+				 Double.parseDouble(x);
+				 numeros.add(x);
+				 
+			 }catch(NumberFormatException nfe){
+				 opera.add(x);
+			 }
+			 
+		 }
+		 
+		 int n = opera.size();
+		 
+		 for (int i=1;i<=n;i+=1) {
+			 String k = opera.remove(opera.size()-1);
+			 numeros.add(k);
+		 }
+		 
+		 for(String i: numeros) {
+			 System.out.print(i);
+		 }
+		 
 	  }
 }
